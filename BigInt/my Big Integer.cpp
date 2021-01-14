5bf65e2361d7371165e3410af2bb87bc89a34418
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2021-01-14 19:05:07 +0600

    Add files via upload

--- BigInt/my Big Integer.cpp
+++ BigInt/my Big Integer.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 typedef long long ll;
 
-#define all(v) v.begin(),v.end()
-
-//The digits are always in reverse order
 class BigInt {
   public:
     string dgt;
@@ -12,119 +9,22 @@ class BigInt {
     BigInt();
     void trim();
     bool isZero();
-    void read(const string&);
-    BigInt(string); //first dgt is the sign/no sign
-    BigInt(ll);
-    BigInt(int);
-    void operator = (const string); //first dgt is the sign/no sign
-    void operator = (const ll);
-    void operator = (const int);
-    friend istream& operator>>(istream&,BigInt&);
-    friend ostream& operator<<(ostream&, const BigInt&);
-    BigInt friend operator + (BigInt,BigInt);
-    BigInt friend operator - (BigInt,BigInt);
-    BigInt friend operator * (BigInt,BigInt);
-    BigInt friend operator / (BigInt,ll);
-    ll friend operator % (BigInt,ll);
+    void read(const string&); //trim
+    BigInt(string); //trim, read
+    BigInt(ll); //trim, read
+    BigInt(int); //trim, read
+    void operator = (const string); //trim, read, BigInt(string)
+    void operator = (const ll); //trim, read, BigInt(ll)
+    void operator = (const int); //trim, read, BigInt(int)
+    friend istream& operator>>(istream&,BigInt&); //trim, read
+    friend ostream& operator<<(ostream&, const BigInt&); //isZero
+    BigInt friend operator + (BigInt,BigInt); //trim, isZero, dgt_comp, dgt_add, dgt_sub
+    BigInt friend operator - (BigInt,BigInt); //trim, isZero, dgt_comp, dgt_add, dgt_sub, +(add operator)
+    BigInt friend operator * (BigInt,BigInt); //trim, dgt_mul
+    BigInt friend operator / (BigInt,ll); //trim, isZero, dgt_div
+    ll friend operator % (BigInt,ll); //isZero, dgt_div
 };
-
-///Must have functions --Start--
-
-BigInt::BigInt() {
-    sign=true;
-    dgt="";
-}
-
-void BigInt::trim() {
-
-    while(!dgt.empty()&&dgt.back()=='0')dgt.pop_back();
-    if(dgt.empty())sign=true;
-}
-
-bool BigInt::isZero() {
-    return dgt.empty()||(dgt.size()==1&&dgt[0]=='0');
-}
-
-void BigInt::read(const string &s) {
-    sign=true;
-    dgt="";
-    ll idx=0,len=s.size();
-    if(s[0]=='+'||s[0]=='-') {
-        if(s[0]=='-')sign=false;
-        idx++;
-        len--;
-    }
-    dgt=s.substr(idx,len);
-    reverse(all(dgt));
-    trim();
-}
-
-///Must have functions --End--
-
-///String Input --Start--
-
-BigInt::BigInt(string s) {
-    read(s);
-}
-
-void BigInt::operator = (const string s) {
-    read(s);
-}
-
-///String input end --End--
-
-///Long Long input --Start--
-
-BigInt::BigInt(ll num) {
-    string s=to_string(num);
-    read(s);
-}
-
-void BigInt::operator = (const ll num) {
-    string s=to_string(num);
-    read(s);
-}
-
-///Long Long input --End--
-
-///Int Input --Start--
-
-BigInt::BigInt(int num) {
-    string s=to_string(num);
-    read(s);
-}
-
-void BigInt::operator = (const int num) {
-    string s=to_string(num);
-    read(s);
-}
-
-///Int Input --End--
-
-///Input-Output Stream --Start--
-
-istream& operator>>(istream &stream,BigInt &v) {
-    string s;
-    stream>>s;
-    v.read(s);
-    return stream;
-}
-
-ostream& operator<<(ostream &stream,BigInt &v) {
-    if(v.isZero()) {
-        stream<<'0';
-        return stream;
-    }
-    if(v.sign==false)stream<<'-';
-    for(ll i=v.dgt.size()-1; i>=0; i--)stream<<v.dgt[i];
-    return stream;
-}
-
-
-///Input-Output Stream --End--
-
-///BigInt Addition and Sunbtruction --Start--
-
+/*----------External Functions Start----------*/
 ll dgt_comp(string a, string b) {
     if(a.size()!=b.size())return (a.size()>b.size())?1:-1;
     for(ll i=a.size()-1; i>=0; i--) {
@@ -132,7 +32,6 @@ ll dgt_comp(string a, string b) {
     }
     return 0;
 }
-
 string dgt_add(string a,string b) {
     string ans="";
     ll alen=a.size(),blen=b.size();
@@ -160,7 +59,6 @@ string dgt_add(string a,string b) {
     }
     return ans;
 }
-
 string dgt_sub(string a, string b) {
     string ans="";
     ll alen=a.size(),blen=b.size();
@@ -192,46 +90,6 @@ string dgt_sub(string a, string b) {
     }
     return ans;
 }
-
-BigInt operator + (BigInt a,BigInt b) {
-    BigInt add;
-    if(a.isZero())return b;
-    else if(b.isZero())return a;
-    if(a.sign==b.sign) {
-        add.sign=a.sign;
-        add.dgt=dgt_add(a.dgt,b.dgt);
-        add.trim();
-        return add;
-    } else {
-        int cmp=dgt_comp(a.dgt,b.dgt);
-        if(cmp==0) {
-            add.sign=true;
-            add.dgt="";
-            return add;
-        } else {
-
-            if(cmp==(-1)) {
-                swap(a,b);
-            }
-            add.sign=a.sign;
-            add.dgt=dgt_sub(a.dgt,b.dgt);
-            add.trim();
-            return add;
-        }
-    }
-
-}
-
-BigInt operator - (BigInt a,BigInt b) {
-    b.sign=!b.sign;
-    BigInt sub=a+b;
-    return sub;
-}
-
-///BigInt Addition and Sunbtruction --End--
-
-///BigInt Multiplication --Start--
-
 string dgt_mul(string a, string b) {
     ll alen=a.size(), blen=b.size(), sz;
     string ans="";
@@ -261,20 +119,6 @@ string dgt_mul(string a, string b) {
     }
     return ans;
 }
-
-BigInt operator * (BigInt a,BigInt b) {
-    BigInt mul;
-    if(a.sign==b.sign)mul.sign=true;
-    else mul.sign=false;
-    mul.dgt=dgt_mul(a.dgt,b.dgt);
-    mul.trim();
-    return mul;
-}
-
-///BigInt Multiplication --End--
-
-///BigInt Divition --Start--
-
 pair<string,ll> dgt_div(string a, ll b) {
     reverse(a.begin(),a.end());
     ll alen=a.size(),idx=0;
@@ -292,7 +136,111 @@ pair<string,ll> dgt_div(string a, ll b) {
 	reverse(ans.begin(),ans.end());
 	return {ans,(ll)tmp};
 }
+/*-----------External Functions End-----------*/
+/*----------Class Functions Start-------------*/
+BigInt::BigInt() {
+    sign=true;
+    dgt="";
+}
+void BigInt::trim() {
 
+    while(!dgt.empty()&&dgt.back()=='0')dgt.pop_back();
+    if(dgt.empty())sign=true;
+}
+bool BigInt::isZero() {
+    return dgt.empty()||(dgt.size()==1&&dgt[0]=='0');
+}
+void BigInt::read(const string &s) {
+    sign=true;
+    dgt="";
+    ll idx=0,len=s.size();
+    if(s[0]=='+'||s[0]=='-') {
+        if(s[0]=='-')sign=false;
+        idx++;
+        len--;
+    }
+    dgt=s.substr(idx,len);
+    reverse(dgt.begin(),dgt.end());
+    trim();
+}
+BigInt::BigInt(string s) {
+    read(s);
+}
+void BigInt::operator = (const string s) {
+    read(s);
+}
+BigInt::BigInt(ll num) {
+    string s=to_string(num);
+    read(s);
+}
+void BigInt::operator = (const ll num) {
+    string s=to_string(num);
+    read(s);
+}
+BigInt::BigInt(int num) {
+    string s=to_string(num);
+    read(s);
+}
+void BigInt::operator = (const int num) {
+    string s=to_string(num);
+    read(s);
+}
+istream& operator>>(istream &stream,BigInt &v) {
+    string s;
+    stream>>s;
+    v.read(s);
+    return stream;
+}
+ostream& operator<<(ostream &stream,BigInt &v) {
+    if(v.isZero()) {
+        stream<<'0';
+        return stream;
+    }
+    if(v.sign==false)stream<<'-';
+    for(ll i=v.dgt.size()-1; i>=0; i--)stream<<v.dgt[i];
+    return stream;
+}
+BigInt operator + (BigInt a,BigInt b) {
+    BigInt add;
+    if(a.isZero())return b;
+    else if(b.isZero())return a;
+    if(a.sign==b.sign) {
+        add.sign=a.sign;
+        add.dgt=dgt_add(a.dgt,b.dgt);
+        add.trim();
+        return add;
+    } else {
+        int cmp=dgt_comp(a.dgt,b.dgt);
+        if(cmp==0) {
+            add.sign=true;
+            add.dgt="";
+            return add;
+        } else {
+
+            if(cmp==(-1)) {
+                swap(a,b);
+            }
+            add.sign=a.sign;
+            add.dgt=dgt_sub(a.dgt,b.dgt);
+            add.trim();
+            return add;
+        }
+    }
+
+}
+BigInt operator - (BigInt a,BigInt b) {
+    b.sign=!b.sign;
+    BigInt sub=a+b;
+    return sub;
+}
+BigInt operator * (BigInt a,BigInt b) {
+    BigInt mul;
+    if(a.sign==b.sign)mul.sign=true;
+    else mul.sign=false;
+    mul.dgt=dgt_mul(a.dgt,b.dgt);
+    mul.trim();
+    return mul;
+}
 BigInt operator / (BigInt a,ll b) {
     BigInt div;
     if(b==0) {
@@ -330,11 +278,7 @@ ll operator % (BigInt a,ll b) {
     if(!sgn)md=md*(-1);
     return md;
 }
-
-///Bigint Divition --End--
-
-
-
+/*-----------Class Functions End--------------*/
 int main() {
     BigInt a;
     ll b;
